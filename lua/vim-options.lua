b4549eea3f8490bb79b2ef8533f3807d330a5d54
Ahmed Gado <ahmedgado0100@gmail.com>
2024-03-08 20:11:34 +0200

    Feat: update vim options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,18 +1,38 @@
 vim.g.mapleader = " "
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+-- column and row limits
+vim.opt.colorcolumn = "80"
+vim.opt.scrolloff = 5
+vim.opt.autochdir = true
 
--- Navigate vim panes better
+-- solve copilot tapp map problem
+vim.g.copilot_assume_mapped = true
+
+-- set up folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- make indents 4 spaces
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+-- make vim commands case insensitive
+vim.opt.ignorecase = true
+
+-- navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
+-- remove search highlight
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
-vim.keymap.set("n", "<leader>r", ":set rnu!<CR>")
+
+-- close current buffer
 vim.keymap.set("n", "<leader>x", ":close<CR>")
 
+-- toggle relative line numbers
+vim.keymap.set("n", "<leader>r", ":set rnu!<CR>")
 vim.wo.number = true
