d500f8bfa7979bc0aff7218b2cb65bc24a730c05
Ahmed Gado <ahmedgado0100@gmail.com>
2024-03-08 18:37:16 +0200

    Feat: add git plugins

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,7 +5,6 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
-
 -- Navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
@@ -13,7 +12,7 @@ vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+vim.keymap.set("n", "<leader>r", ":set rnu!<CR>")
 vim.keymap.set("n", "<leader>x", ":close<CR>")
 
 vim.wo.number = true
-vim.wo.relativenumber = true
