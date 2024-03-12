90f579ebe758d3fd79f4ec140cbd9f207a94bc0b
Ahmed Gado <ahmedgado0100@gmail.com>
2024-03-12 14:00:00 +0200

    Feat: add rull_lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,15 +1,23 @@
 return {
-	"neovim/nvim-lspconfig",
-	lazy = false,
-	config = function()
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		local lspconfig = require("lspconfig")
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-		})
-		lspconfig.pyright.setup({
-			capabilities = capabilities,
-		})
-	end,
+        local lspconfig = require("lspconfig")
+        lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+            filetypes = { "lua" },
+        })
+
+        lspconfig.pyright.setup({
+            capabilities = capabilities,
+            filetypes = { "python" },
+        })
+
+        lspconfig.ruff_lsp.setup({
+            capabilities = capabilities,
+            filetypes = { "python" },
+        })
+    end,
 }
