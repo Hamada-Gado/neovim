8dd142bb4b6eb86ae51c621f382a2d4de9f1405d
Ahmed Gado <ahmedgado0100@gmail.com>
2024-08-21 20:10:12 +0300

    add autotag completion for html tags

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_installed = { "lua", "python" },
-            auto_install = true,
-            ignore_install = { "tmux" },
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = { "lua", "python", "javascript", "typescript", "tsx" },
+			auto_install = true,
+			ignore_install = { "tmux" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
